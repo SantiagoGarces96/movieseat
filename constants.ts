75bda3d3ca5393314e91821f5757648c804110c8
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2024-08-28 03:26:17 -0500

    add: TMDB_API_URL_2

--- constants.ts
+++ constants.ts
@@ -1,2 +1,3 @@
 export const TMDB_API_URL = "https://api.themoviedb.org/3/movie";
+export const TMDB_API_URL_2 = "https://api.themoviedb.org/3/discover";
 export const OMDB_API_URL = "http://www.omdbapi.com";
