b99b248b1e17cd0b601fba0e6b808648d803ec81
YepileF <yepilef@gmail.com>
2024-09-10 09:13:50 -0500

    enable: cron job

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,34 +1,28 @@
-import type { NextApiResponse } from "next";
 import dbConnect from "@/lib/dbConnect";
 import { getNowPlayingTMDB, getUpcomingTMDB } from "@/lib/TMDB";
 import Movie from "@/models/Movie";
 import Session from "@/models/Session";
 import { NextRequest, NextResponse } from "next/server";
 
-// export default async function handler(req: Request, res: NextApiResponse) {
-//   // Verify if the request comes with the correct authorization.
-//   if (
-//     req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
-//   ) {
-//     return res.status(401).end("Unauthorized");
-//   }
-
-//   await dbConnect();
-
-//   try {
-//     await Movie.deleteMany({});
-//     await Session.deleteMany({});
-//     await getNowPlayingTMDB();
-//     await getUpcomingTMDB();
-
-//     console.log("Cron Job Executed !!!");
-//     return res.status(200).end("Fullfilled DB with new data.");
-//   } catch (error: any) {
-//     return res.status(500).end(error.message || "An error occurred");
-//   }
-// }
-
 export async function GET(req: NextRequest) {
-  console.log("Execute cron at: " + new Date());
-  return new NextResponse("Cron ran.", { status: 200 });
+  console.log("Execute cron at: ", new Date());
+  if (
+    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+
+  try {
+    await dbConnect();
+    await Movie.deleteMany({});
+    await Session.deleteMany({});
+    await getNowPlayingTMDB();
+    await getUpcomingTMDB();
+
+    return new NextResponse("Fullfilled DB with new data.", { status: 200 });
+  } catch (error: any) {
+    return new NextResponse(error.message || "An error occurred", {
+      status: 500,
+    });
+  }
 }
