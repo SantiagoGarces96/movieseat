5543a25890b4bf823f24f6fe6318a3a1a9b576b5
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2024-09-11 18:24:51 -0500

    fix: layout and page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-"use client";
-import Hero from './components/home/Hero';
-import Features from './components/home/Features';
 
 const Home = () => {
   return (
     <div>
       <main className="min-h-screen bg-gray-100">
-        <Hero />
-        <Features />
       </main>
     </div>
   );
