741be2ce6353da3217349c6308055584623d6c9a
YepileF <yepilef@gmail.com>
2024-09-11 10:57:44 -0500

    add: primary colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#FFFDF6",
+        secondary: "#1E91D6",
+        accent: "#0072BB",
       },
     },
   },
