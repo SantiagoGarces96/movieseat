e6cf500d798753b7f5c147022add080d2f612272
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2024-09-11 15:35:12 -0500

    solve: conficts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-
 const Home = () => {
   return (
     <div>
-      <main className="min-h-screen bg-gray-100">
-      </main>
+      <main className="min-h-screen bg-gray-100"></main>
     </div>
   );
 };
