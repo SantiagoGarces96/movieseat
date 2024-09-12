77d7d3660df806ca36a4f23a2721fb5a1640c0d7
YepileF <yepilef@gmail.com>
2024-09-12 12:19:22 -0500

    add: side navbar

--- app/ui/dashboard/NavBar/NavLinks.tsx
+++ app/ui/dashboard/NavBar/NavLinks.tsx
@@ -1,49 +1,35 @@
 "use client";
-
-import {
-  HiOutlineDocumentDuplicate,
-  HiOutlineHome,
-  HiOutlineUserGroup,
-} from "react-icons/hi2";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
-
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
-const links = [
-  { name: "Home", href: "/dashboard", icon: HiOutlineHome },
-  {
-    name: "Invoices",
-    href: "/dashboard/invoices",
-    icon: HiOutlineDocumentDuplicate,
-  },
-  { name: "Customers", href: "/dashboard/customers", icon: HiOutlineUserGroup },
-];
+import { navLinks } from "@/constants/dashboard/navLinks";
 
 export default function NavLinks() {
   const pathname = usePathname();
 
   return (
-    <>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
+    <ul className="menu menu-md w-full rounded-box bg-base-100">
+      {navLinks.map((link) => {
+        if (link.icon && link.href) {
+          const LinkIcon = link.icon;
+          return (
+            <li key={link.name} className="mb-3">
+              <Link
+                href={link.href}
+                className={clsx("", { "bg-accent/50": pathname === link.href })}
+              >
+                <LinkIcon className="h-6 w-6" />
+                <span className="text-lg font-light">{link.name}</span>
+              </Link>
+            </li>
+          );
+        }
         return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-              {
-                "bg-sky-100 text-blue-600": pathname === link.href,
-              },
-            )}
-          >
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </Link>
+          <li key={link.name} className="menu-title">
+            {link.name}
+          </li>
         );
       })}
-    </>
+    </ul>
   );
 }
