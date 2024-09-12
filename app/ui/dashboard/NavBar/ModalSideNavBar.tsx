02e057034cef54e00b47454aeae2741abb18fcae
YepileF <yepilef@gmail.com>
2024-09-12 15:09:04 -0500

    fix: positon side navbar

--- app/ui/dashboard/NavBar/ModalSideNavBar.tsx
+++ app/ui/dashboard/NavBar/ModalSideNavBar.tsx
@@ -3,7 +3,7 @@ import SideNav from "./SideNavBar";
 export default function ModalSideNavBar() {
   return (
     <dialog id="modal_side_navbar" className="modal modal-top">
-      <div className="modal-box h-[90rem] w-[20rem] rounded-none !p-0">
+      <div className="absolute h-full w-[20rem] rounded-none bg-base-100">
         <SideNav />
       </div>
       <form method="dialog" className="modal-backdrop">
