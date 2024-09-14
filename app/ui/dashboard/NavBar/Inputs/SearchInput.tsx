e88ebf0a7e27470afc5a4161f3d55be5b3d6a4f7
YepileF <yepilef@gmail.com>
2024-09-13 20:20:15 -0500

    feat: url paramr

--- app/ui/dashboard/NavBar/Inputs/SearchInput.tsx
+++ app/ui/dashboard/NavBar/Inputs/SearchInput.tsx
@@ -1,10 +1,35 @@
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { HiMagnifyingGlass } from "react-icons/hi2";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function SearchInput() {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <label className="input input-sm input-bordered flex w-full items-center gap-2">
       <HiMagnifyingGlass className="h-5 w-5" />
-      <input type="text" className="grow" placeholder="Search" />
+      <input
+        type="text"
+        className="grow"
+        placeholder="Search"
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get("query")?.toString()}
+      />
     </label>
   );
 }
