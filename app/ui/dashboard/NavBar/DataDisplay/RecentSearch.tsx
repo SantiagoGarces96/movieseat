5904094a301360f804799c1487d0032cc9a7001d
YepileF <yepilef@gmail.com>
2024-09-14 11:09:09 -0500

    fix: get recents searchs

--- app/ui/dashboard/NavBar/DataDisplay/RecentSearch.tsx
+++ app/ui/dashboard/NavBar/DataDisplay/RecentSearch.tsx
@@ -1,23 +1,42 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import Badge from "./Badge";
 import { getRecentSearch } from "@/utils/localStorage";
 
 export default function RecentSearch() {
   const [recentSearchData, setRecentSearchData] = useState<string[]>([]);
 
+  const loadRecentSearches = () => {
+    const recentSearches = getRecentSearch();
+    setRecentSearchData(recentSearches);
+  };
+
   useEffect(() => {
-    const data = getRecentSearch();
-    setRecentSearchData(data);
+    loadRecentSearches();
+    const handleRecentSearchUpdate = () => {
+      loadRecentSearches();
+    };
+    window.addEventListener("recentSearchUpdated", handleRecentSearchUpdate);
+
+    return () => {
+      window.removeEventListener(
+        "recentSearchUpdated",
+        handleRecentSearchUpdate,
+      );
+    };
   }, []);
 
   return (
     <div className="flex flex-col gap-3 p-5">
       <span className="text-sm font-medium text-gray-400">Recent Search</span>
       <div className="flex flex-grow gap-2">
-        {recentSearchData.map((label, index) => (
-          <Badge key={label + index} label={label} />
-        ))}
+        {recentSearchData.length > 0 ? (
+          recentSearchData.map((label, index) => (
+            <Badge key={label + index} label={label} />
+          ))
+        ) : (
+          <span className="text-gray-500">No recent searches.</span>
+        )}
       </div>
     </div>
   );
--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -5,12 +5,15 @@ export const recentSearchSaver = (query: string): void => {
     );
     recentSearches.unshift(query);
     const limitedSearches = recentSearches.slice(0, 4);
-    console.log(limitedSearches);
-
     localStorage.setItem("recentSearches", JSON.stringify(limitedSearches));
+    const event = new CustomEvent("recentSearchUpdated");
+    window.dispatchEvent(event);
   }
 };
 
 export const getRecentSearch = (): string[] => {
+  if (typeof window === "undefined") {
+    return [];
+  }
   return JSON.parse(localStorage.getItem("recentSearches") || "[]");
 };
