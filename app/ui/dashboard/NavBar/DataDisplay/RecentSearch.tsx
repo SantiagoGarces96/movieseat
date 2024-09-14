aee2d664e8bd5a64b3e3bbfc687a1b52f76cae24
YepileF <yepilef@gmail.com>
2024-09-14 10:26:36 -0500

    fix: localStorage is not defined

--- app/ui/dashboard/NavBar/DataDisplay/RecentSearch.tsx
+++ app/ui/dashboard/NavBar/DataDisplay/RecentSearch.tsx
@@ -1,15 +1,21 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Badge from "./Badge";
 import { getRecentSearch } from "@/utils/localStorage";
 
 export default function RecentSearch() {
-  const recetSearchData = getRecentSearch();
+  const [recentSearchData, setRecentSearchData] = useState<string[]>([]);
+
+  useEffect(() => {
+    const data = getRecentSearch();
+    setRecentSearchData(data);
+  }, []);
+
   return (
     <div className="flex flex-col gap-3 p-5">
       <span className="text-sm font-medium text-gray-400">Recent Search</span>
       <div className="flex flex-grow gap-2">
-        {recetSearchData.map((label, index) => (
+        {recentSearchData.map((label, index) => (
           <Badge key={label + index} label={label} />
         ))}
       </div>
