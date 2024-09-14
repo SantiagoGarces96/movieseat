68d1e62808e7206fb6855b208eccc73b7a2c2919
YepileF <yepilef@gmail.com>
2024-09-14 08:56:03 -0500

    feat: search by recent

--- app/ui/dashboard/NavBar/DataDisplay/Badge.tsx
+++ app/ui/dashboard/NavBar/DataDisplay/Badge.tsx
@@ -1,8 +1,23 @@
+"use client";
 import { IBadgeDashboard } from "@/interfaces/dasboard";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function Badge({ label }: IBadgeDashboard) {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const handleClick = () => {
+    const params = new URLSearchParams(searchParams);
+    params.set("query", label);
+    replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
-    <div className="badge badge-ghost badge-lg p-3 text-xs font-normal text-gray-500">
+    <div
+      className="badge badge-ghost badge-lg cursor-pointer p-3 text-xs font-normal text-gray-500 hover:bg-accent hover:text-white"
+      onClick={handleClick}
+    >
       {label}
     </div>
   );
