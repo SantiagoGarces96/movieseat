9b4bdc0c0135b4f6fe276252c17ede2be2ff97f3
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2024-09-15 03:12:15 -0500

    add: image.tmdb.org

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
       { protocol: "https", hostname: "m.media-amazon.com" },
       { protocol: "https", hostname: "archivos-cms.cinecolombia.com" },
+      { protocol: "https", hostname: "image.tmdb.org" },
     ],
   },
 };
