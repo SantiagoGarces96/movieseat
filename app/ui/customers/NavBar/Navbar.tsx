a1d25cf9b4564aee57ed4be87b48160888a75ea4
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2024-09-15 03:17:50 -0500

    refactor: navbar

--- app/ui/customers/NavBar/Navbar.tsx
+++ app/ui/customers/NavBar/Navbar.tsx
@@ -1,45 +1,20 @@
-"use client";
-import { signIn, signOut, useSession } from 'next-auth/react';
+import React from "react";
 
 const Navbar = () => {
-  // const { data: session } = useSession();
-
-  // if (status === 'loading') {
-  //   return null; // O bien, un placeholder para evitar cambios entre el servidor y el cliente
-  // }
-
   return (
-    <nav className="bg-gray-800 p-4 flex justify-between items-center">
-      {/* Logo */}
-      <div className="text-white font-bold text-xl">
-        <a href="/">MovieSeat</a>
-      </div>
-
-      {/* Links */}
-      <ul className="flex space-x-4 text-white">
-        <li><a href="/releases">Estrenos</a></li>
-        <li><a href="/upcoming">Proximos a estrenar</a></li>
-        <li><a href="/foods">Comidas</a></li>
-        <li><a href="/about">Acerca de</a></li>
-      </ul>
-
-      {/* Login / Logout */}
-      <div>
-        {/* {session ? (
-          <button
-            onClick={() => signOut()}
-            className="bg-red-500 px-4 py-2 text-white rounded-md"
-          >
-            Logout
-          </button>
-        ) : (
-          <button
-            onClick={() => signIn()}
-            className="bg-blue-500 px-4 py-2 text-white rounded-md"
-          >
-            Login
-          </button>
-        )} */}
+    <nav className="p-4 bg-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-2xl font-bold">MovieSeat</div>
+        <div className="flex gap-6">
+          <a href="/home" className="hover:text-[#0072BB]">Home</a>
+          <a href="/releases" className="hover:text-[#0072BB]">Releases</a>
+          <a href="/upcoming" className="hover:text-[#0072BB]">Upcoming</a>
+          <a href="/foods" className="hover:text-[#0072BB]">Snacks</a>
+          <a href="/about" className="hover:text-[#0072BB]">About Us</a>
+        </div>
+        <div>
+          <button className="bg-[#0072BB] text-[#FFFDF6] px-4 py-2 rounded">Login</button>
+        </div>
       </div>
     </nav>
   );
