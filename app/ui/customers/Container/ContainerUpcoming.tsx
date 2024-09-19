8dea16858c130547c58d577c3e860050889c1c0e
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2024-09-18 23:27:22 -0500

    change: containers desing

--- app/ui/customers/Container/ContainerUpcoming.tsx
+++ app/ui/customers/Container/ContainerUpcoming.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import MovieCard from "@/app/ui/customers/card/MovieCard";
 import { IMovie } from "@/interfaces/movie";
 import { MovieStatus } from "@/types/movie";
@@ -9,9 +10,8 @@ interface PreSaleUpcomingBoxProps {
 
 const PreSaleUpcomingBox: React.FC<PreSaleUpcomingBoxProps> = ({ movies }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const moviesToShow = 6;
-  const cardWidth = 300;
-  const cardHeight = 550;
+  const [moviesToShow, setMoviesToShow] = useState(4);
+  const [cardSize, setCardSize] = useState({ width: 300, height: 550 });
 
   const filteredMovies = movies
     .filter(
@@ -19,7 +19,39 @@ const PreSaleUpcomingBox: React.FC<PreSaleUpcomingBoxProps> = ({ movies }) => {
         movie.status === MovieStatus.PRE_SALE ||
         movie.status === MovieStatus.UPCOMING
     )
-    .sort((a, b) => new Date(a.releaseDate).getTime() - new Date(b.releaseDate).getTime());
+    .sort(
+      (a, b) => new Date(a.releaseDate).getTime() - new Date(b.releaseDate).getTime()
+    );
+
+  const updateMoviesToShow = () => {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth >= 3840) {
+      setMoviesToShow(8); 
+      setCardSize({ width: 320, height: 600 });
+    } else if (screenWidth >= 1920) {
+      setMoviesToShow(6); 
+      setCardSize({ width: 300, height: 550 });
+    } else if (screenWidth >= 1280) {
+      setMoviesToShow(4); 
+      setCardSize({ width: 280, height: 500 });
+    } else if (screenWidth >= 720) {
+      setMoviesToShow(3); 
+      setCardSize({ width: 230, height: 430 });
+    } else if (screenWidth >= 375) {
+      setMoviesToShow(2);
+      setCardSize({ width: 180, height: 400 });
+    } else {
+      setMoviesToShow(1);
+      setCardSize({ width: 150, height: 330 });
+    }
+  };
+
+  useEffect(() => {
+    updateMoviesToShow();
+    window.addEventListener("resize", updateMoviesToShow);
+    return () => window.removeEventListener("resize", updateMoviesToShow);
+  }, []);
 
   const nextPage = () => {
     if (currentIndex + moviesToShow < filteredMovies.length) {
@@ -40,20 +72,22 @@ const PreSaleUpcomingBox: React.FC<PreSaleUpcomingBoxProps> = ({ movies }) => {
 
   return (
     <div className="relative flex items-center overflow-hidden">
+      {/* Botón anterior fuera de las tarjetas */}
       <button
-        onClick={prevPage}
+       onClick={prevPage}
         className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white rounded-full p-2 z-10"
         disabled={currentIndex === 0}
       >
         &lt;
       </button>
 
-      <div className="flex space-x-4 py-4">
+      {/* Contenedor de tarjetas de películas */}
+      <div className="flex justify-center space-x-2 py-2 w-full px-8">
         {currentMovies.map((movie) => (
           <div
             key={movie._id}
             className="flex-shrink-0"
-            style={{ width: cardWidth, height: cardHeight }}
+             style={{ width: cardSize.width, height: cardSize.height }}
           >
             <MovieCard
               id={movie._id}
@@ -69,8 +103,9 @@ const PreSaleUpcomingBox: React.FC<PreSaleUpcomingBoxProps> = ({ movies }) => {
         ))}
       </div>
 
+      {/* Botón siguiente fuera de las tarjetas */}
       <button
-        onClick={nextPage}
+          onClick={nextPage}
         className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white rounded-full p-2 z-10"
         disabled={currentIndex + moviesToShow >= filteredMovies.length}
       >
