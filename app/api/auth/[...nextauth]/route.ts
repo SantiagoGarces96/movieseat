16cfc64bd888f43509866dbd22bbaa676e7dc187
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2024-09-20 03:39:47 -0500

    fix: next_auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,13 @@
 import NextAuth from "next-auth";
-import { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google"; // Ejemplo con Google
+import GoogleProvider from "next-auth/providers/google";
 
-// export const authOptions: NextAuthOptions = {
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-//     // Puedes añadir más proveedores como GitHub, Facebook, etc.
-//   ],
-//   callbacks: {
-//     async session({ session, token, user }) {
-//       // Puedes agregar lógica personalizada aquí, como token o datos del usuario.
-//       session.user.id = token.sub; // Ejemplo de añadir el ID del usuario a la sesión
-//       return session;
-//     },
-//   },
-// };
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+});
 
-// const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
+export { handler as Get, handler as Post };
