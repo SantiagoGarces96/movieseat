4cb3f22bc184548ccf0f16e6fdf54b943c8fcb25
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2024-09-25 15:49:16 -0500

    test: updatemoviestatus

--- app/api/filldb/route.ts
+++ app/api/filldb/route.ts
@@ -5,25 +5,23 @@ import {
   updateMoviesStatus,
 } from "@/lib/TMDB";
 
-export const dynamic = "force-dynamic"; // static by default, unless reading the request
+export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
-  // const authHeader = request.headers.get("authorization");
-  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-  //   return new Response("Unauthorized", {
-  //     status: 401,
-  //   });
-  // }
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
   try {
     await dbConnect();
     await getNowPlayingTMDB();
     await getUpcomingTMDB();
-    // await updateMoviesStatus();
-    console.log("Fullfilled DB with new data.");
+    await updateMoviesStatus();
     return new Response("Fullfilled DB with new data.", { status: 200 });
   } catch (error: any) {
     console.log(error);
-
     return new Response(error.message || "An error occurred", {
       status: 500,
     });
