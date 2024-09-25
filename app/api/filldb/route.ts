7f3ef35dd4e7ceacf30af25cf6b7d0651e1c2537
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2024-09-24 19:20:54 -0500

    feat: schedule cron job

---
+++ app/api/filldb/route.ts
@@ -0,0 +1,29 @@
+import dbConnect from "@/lib/dbConnect";
+import {
+  getNowPlayingTMDB,
+  getUpcomingTMDB,
+  updateMoviesStatus,
+} from "@/lib/TMDB";
+
+export const dynamic = "force-dynamic"; // static by default, unless reading the request
+
+export async function GET(request: Request) {
+  // const authHeader = request.headers.get("authorization");
+  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  //   return new Response("Unauthorized", {
+  //     status: 401,
+  //   });
+  // }
+  try {
+    await dbConnect();
+    await getNowPlayingTMDB();
+    await getUpcomingTMDB();
+    await updateMoviesStatus();
+    console.log("Fullfilled DB with new data.");
+    return new Response("Fullfilled DB with new data.", { status: 200 });
+  } catch (error: any) {
+    return new Response(error.message || "An error occurred", {
+      status: 500,
+    });
+  }
+}
