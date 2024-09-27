f618348cc9122e083f1704631177f865da1bdfa9
YepileF <yepilef@gmail.com>
2024-09-27 16:45:52 -0500

    fix: upcoming card

--- app/ui/customers/upcoming/moviesCards/moviesCards.tsx
+++ app/ui/customers/upcoming/moviesCards/moviesCards.tsx
@@ -9,7 +9,9 @@ interface PaginatedMoviesGridProps {
   movies: IMovie[];
 }
 
-const PaginatedMoviesGrid: React.FC<PaginatedMoviesGridProps> = ({ movies }) => {
+const PaginatedMoviesGrid: React.FC<PaginatedMoviesGridProps> = ({
+  movies,
+}) => {
   const [currentPage, setCurrentPage] = useState(0);
   const [cardSize, setCardSize] = useState({ width: 330, height: 580 });
   const moviesPerPage = 12;
@@ -18,10 +20,11 @@ const PaginatedMoviesGrid: React.FC<PaginatedMoviesGridProps> = ({ movies }) => 
     .filter(
       (movie) =>
         movie.status === MovieStatus.PRE_SALE ||
-        movie.status === MovieStatus.UPCOMING
+        movie.status === MovieStatus.UPCOMING,
     )
     .sort(
-      (a, b) => new Date(a.releaseDate).getTime() - new Date(b.releaseDate).getTime()
+      (a, b) =>
+        new Date(a.releaseDate).getTime() - new Date(b.releaseDate).getTime(),
     );
 
   const totalPages = Math.ceil(filteredMovies.length / moviesPerPage);
@@ -40,38 +43,41 @@ const PaginatedMoviesGrid: React.FC<PaginatedMoviesGridProps> = ({ movies }) => 
 
   const currentMovies = filteredMovies.slice(
     currentPage * moviesPerPage,
-    (currentPage + 1) * moviesPerPage
+    (currentPage + 1) * moviesPerPage,
   );
 
   return (
     <div className="w-full">
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      {currentMovies.map((movie) => (
-        <div key={movie._id} style={{ width: cardSize.width, height: cardSize.height }} className="transition-transform transform hover:scale-105">
-          <MovieCard
-            id={movie._id}
-            title={movie.title}
-            poster={movie.poster}
-            backdrop={movie.backdrop}
-            status={movie.status}
-            duration={movie.duration}
-            releaseDate={new Date(movie.releaseDate).toLocaleDateString()}
-            genre={movie.genre}
-          />
-        </div>
-      ))}
-    </div>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {currentMovies.map((movie) => (
+          <div
+            key={movie._id}
+            style={{ width: cardSize.width, height: cardSize.height }}
+            className="transform transition-transform hover:scale-105"
+          >
+            <MovieCard
+              id={movie._id}
+              title={movie.title}
+              poster={movie.poster}
+              status={movie.status}
+              duration={movie.duration}
+              releaseDate={movie.releaseDate}
+              genre={movie.genre}
+            />
+          </div>
+        ))}
+      </div>
 
-    {totalPages > 1 && (
-      <PaginationControls
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onNextPage={handleNextPage}
-        onPrevPage={handlePrevPage}
-        onPageChange={(page) => setCurrentPage(page)}
-      />
-    )}
-  </div>
-);
+      {totalPages > 1 && (
+        <PaginationControls
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onNextPage={handleNextPage}
+          onPrevPage={handlePrevPage}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      )}
+    </div>
+  );
 };
 export default PaginatedMoviesGrid;
