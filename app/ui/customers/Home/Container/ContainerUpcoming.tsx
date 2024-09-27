95e3850cece6103b65e3c9904caf2fb10aaec8b8
YepileF <yepilef@gmail.com>
2024-09-27 16:43:26 -0500

    fix: movie card

--- app/ui/customers/Home/Container/ContainerUpcoming.tsx
+++ app/ui/customers/Home/Container/ContainerUpcoming.tsx
@@ -10,35 +10,38 @@ interface PreSaleUpcomingBoxProps {
   currentPage: number;
 }
 
-const PreSaleUpcomingBox: React.FC<PreSaleUpcomingBoxProps> = ({ movies, currentPage }) => {
+const PreSaleUpcomingBox: React.FC<PreSaleUpcomingBoxProps> = ({
+  movies,
+  currentPage,
+}) => {
   const moviesPerPage = 5;
 
   const filteredMovies = movies
     .filter(
       (movie) =>
         movie.status === MovieStatus.PRE_SALE ||
-        movie.status === MovieStatus.UPCOMING
+        movie.status === MovieStatus.UPCOMING,
     )
     .sort(
       (a, b) =>
-        new Date(a.releaseDate).getTime() - new Date(b.releaseDate).getTime()
+        new Date(a.releaseDate).getTime() - new Date(b.releaseDate).getTime(),
     );
 
   const totalPages = Math.ceil(filteredMovies.length / moviesPerPage);
   const startIndex = (currentPage - 1) * moviesPerPage;
   const currentMovies = filteredMovies.slice(
     startIndex,
-    startIndex + moviesPerPage
+    startIndex + moviesPerPage,
   );
 
   return (
     <div className="relative flex flex-col items-center overflow-hidden">
       {/* Grilla para pantallas grandes */}
-      <div className="hidden sm:flex flex-wrap justify-center gap-4 py-4 w-full px-8">
+      <div className="hidden w-full flex-wrap justify-center gap-4 px-8 py-4 sm:flex">
         {currentMovies.map((movie) => (
           <div
             key={movie._id}
-            className="transition-transform transform hover:scale-105 w-full sm:w-1/2 hd:w-1/3 fhd:w-1/6 2k:w-1/8 max-w-xs"
+            className="hd:w-1/3 fhd:w-1/6 2k:w-1/8 w-full max-w-xs transform transition-transform hover:scale-105 sm:w-1/2"
           >
             <MovieCard
               id={movie._id}
@@ -54,7 +57,7 @@ const PreSaleUpcomingBox: React.FC<PreSaleUpcomingBoxProps> = ({ movies, current
       </div>
 
       {/* Vista para móviles */}
-      <div className="block sm:hidden w-full px-4">
+      <div className="block w-full px-4 sm:hidden">
         {currentMovies.map((movie) => (
           <div key={movie._id} className="mb-4">
             <MobileMovieCard
@@ -69,8 +72,8 @@ const PreSaleUpcomingBox: React.FC<PreSaleUpcomingBoxProps> = ({ movies, current
           </div>
         ))}
         {/* Botón de ver más solo para móviles */}
-        <div className="flex justify-center mt-4">
-          <button className="text-blue-500 font-semibold underline">
+        <div className="mt-4 flex justify-center">
+          <button className="font-semibold text-blue-500 underline">
             Ver más
           </button>
         </div>
