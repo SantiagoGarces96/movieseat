7fa533e0fc009fa24bb1dd1d770e151541217964
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2024-10-03 15:39:26 -0500

    add: screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         secondary: "#1E91D6",
         accent: "#0072BB",
       },
+      screens: {
+        sm: "460px", // Minimum width for mobile devices
+        md: "768px", // Minimum width for tablets
+        hd: "1280px", // Minimum width for HD screens (720p)
+        fhd: "1920px", // Minimum width for Full HD screens (1080p)
+        "2k": "2400px", // Minimum width for 2K screens
+      },
     },
   },
   daisyui: {
