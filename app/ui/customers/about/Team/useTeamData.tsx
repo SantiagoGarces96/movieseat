ef54d8f0fc8b59f756adcb8bcd40c08a4a385da2
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2024-10-17 11:53:06 -0500

    add: work team

---
+++ app/ui/customers/about/Team/useTeamData.tsx
@@ -0,0 +1,27 @@
+export const getTeamData = () => {
+  return [
+    {
+      role: "Desarrollador Full Stack",
+      name: "Felipe Yepes",
+      photo: "/images/Felipe.webp",
+      skills:
+        "Felipe Yepes es un desarrollador full stack versátil con una pasión por construir aplicaciones web dinámicas y robustas. Su experiencia abarca tanto tecnologías frontend como backend.",
+      urls: {
+        linkedin: "https://www.linkedin.com/in/yepesf",
+        github: "https://github.com/YepesF",
+        gitlab: "https://gitlab.com/YepileF",
+      },
+    },
+    {
+      role: "Desarrollador Full Stack",
+      name: "Santiago Aguirre",
+      photo: "/images/Santiago.jpg",
+      skills:
+        "Santiago Aguirre es un desarrollador full stack experimentado conocido por crear aplicaciones web fluidas y receptivas, dominando tanto el desarrollo frontend como backend.",
+      urls: {
+        linkedin: "https://www.linkedin.com/in/santiagogarces01",
+        github: "https://github.com/SantiagoGarces96",
+      },
+    },
+  ];
+};
