1cc9066779cf25b4af250db6b2e3dde6a078d510
YepileF <yepilef@gmail.com>
2024-10-23 10:13:04 -0500

    handle: errors

--- app/dashboard/[section]/not-found.tsx
+++ app/dashboard/[section]/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
     <main className="flex h-full flex-col items-center justify-center gap-2">
       <HiOutlineFaceFrown className="h-40 w-40 text-gray-400" />
       <h2 className="text-xl font-semibold">404 No Encontrado</h2>
-      <p>No se ha encontrado el recurso solicitado.</p>
+      <p>No se ha encontrado la pagina solicitada.</p>
       <button
         type="button"
         onClick={router.back}
