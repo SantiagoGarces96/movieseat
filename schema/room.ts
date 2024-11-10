35c9092d88425eb038f65eebf7cb31623cfbef07
YepileF <yepilef@gmail.com>
2024-11-10 10:50:16 -0500

    rename: room fiel

--- schema/room.ts
+++ schema/room.ts
@@ -13,7 +13,7 @@ export const roomSchema = z.object({
       required_error: "El título es obligatorio",
     })
     .min(1, "El título no puede estar vacío"),
-  room: SessionRoomEnum.default(SessionRoom["2D"]),
+  roomType: SessionRoomEnum.default(SessionRoom["2D"]),
   totalSeatsPreferential: z
     .number({
       required_error: "El Total asientos preferenciales es obligatorio",
