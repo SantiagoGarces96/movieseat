c6e4294613939fdc48d926f29807cf2eab4dc274
YepileF <yepilef@gmail.com>
2024-11-10 11:15:06 -0500

    feat: create room form

--- app/ui/dashboard/Rooms/components/Create/components/CreateForm.tsx
+++ app/ui/dashboard/Rooms/components/Create/components/CreateForm.tsx
@@ -1,68 +1,16 @@
 "use client";
-import { IMovie } from "@/interfaces/movie";
-import { IRoom } from "@/interfaces/room";
-import { createSession, getAvailableSessionTimes } from "@/services/sessions";
 import Link from "next/link";
-import { useState } from "react";
 import { useFormState } from "react-dom";
-import { cn } from "@/utils/cn";
 import { initialState } from "@/constants/dashboard/form";
 import useAlert from "@/app/hooks/useAlert";
 import Alert from "../../../../Alert";
 import { Button } from "../../../../Button";
+import { SessionRoom } from "@/types/session";
+import { createRooom } from "@/services/rooms";
 
-export default function SessionCreateForm({
-  movies,
-  rooms,
-}: {
-  movies: IMovie[];
-  rooms: IRoom[];
-}) {
-  const [seats, setSeats] = useState({
-    totalSeats: 0,
-    totalSeatsPreferential: 0,
-    totalSeatsGeneral: 0,
-  });
-  const [roomId, setRoomId] = useState<string>("");
-  const [currentDate, setCurrentDate] = useState<string>("");
-  const [disableDate, setDisableDate] = useState<boolean>(true);
-  const [notAvailableTimes, setNotAvailableTimes] = useState<boolean>(false);
-  const [availableSessionTimes, setAvailableSessionTimes] = useState<string[]>(
-    [],
-  );
-  const [currentTime, setCurrentTime] = useState<string>("");
-
-  const [state, formAction] = useFormState(
-    createSession.bind(null, currentTime),
-    initialState,
-  );
-
+export default function RoomCreateForm() {
+  const [state, formAction] = useFormState(createRooom, initialState);
   const { showAlert } = useAlert(state);
-
-  const handleRoom = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const currentRoomId = e.target.value;
-    setCurrentDate("");
-    setAvailableSessionTimes([]);
-    setRoomId(currentRoomId);
-    setDisableDate(false);
-    const currentRoom = rooms.find(
-      ({ _id }) => _id.toString() === currentRoomId,
-    );
-    if (currentRoom) {
-      const { totalSeats, totalSeatsPreferential, totalSeatsGeneral } =
-        currentRoom;
-      setSeats({ totalSeats, totalSeatsPreferential, totalSeatsGeneral });
-    }
-  };
-
-  const handleDate = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const date = e.target.value;
-    setCurrentDate(date);
-    const availableSessions = await getAvailableSessionTimes(date, roomId);
-    setAvailableSessionTimes(availableSessions);
-    setNotAvailableTimes(!!date && availableSessions.length === 0);
-  };
-
   return (
     <form
       action={formAction}
@@ -71,159 +19,65 @@ export default function SessionCreateForm({
       {showAlert && <Alert {...state} />}
       <label className="form-control col-span-12 grid w-full lg:col-span-6">
         <div className="label text-lg font-bold">
-          <span className="label-text">Película</span>
+          <span className="label-text">Nombre de la sala</span>
         </div>
-        <select
-          id="movie"
-          name="movieId"
-          className="select select-bordered select-sm w-full"
-          defaultValue=""
+        <input
+          id="name"
+          name="name"
+          type="text"
+          className="input input-sm input-bordered w-full"
           required
-        >
-          <option value="" disabled>
-            Seleccione una película
-          </option>
-          {movies.map(({ _id, title }) => (
-            <option key={_id} value={_id}>
-              {title}
-            </option>
-          ))}
-        </select>
+        />
       </label>
-
       <label className="form-control col-span-12 grid w-full lg:col-span-6">
         <div className="label text-lg font-bold">
-          <span className="label-text">Sala</span>
+          <span className="label-text">Tipo de sala</span>
         </div>
         <select
           id="room"
-          name="roomId"
+          name="room"
           className="select select-bordered select-sm w-full"
           defaultValue=""
-          onChange={handleRoom}
           required
         >
           <option value="" disabled>
-            Seleccione una sala
+            Seleccione un tipo
           </option>
-          {rooms.map(({ _id, name }) => (
-            <option key={_id.toString()} value={_id.toString()}>
-              {name}
+          {Object.entries(SessionRoom).map(([_, value], index) => (
+            <option key={value + index} value={value}>
+              {value}
             </option>
           ))}
         </select>
       </label>
-
-      <label className="form-control col-span-12 grid w-full lg:col-span-12">
-        <div className="label text-lg font-bold">
-          <span className="label-text">Fecha de la función</span>
-        </div>
-        <input
-          id="date"
-          name="date"
-          type="date"
-          className="input input-sm input-bordered w-full"
-          required
-          value={currentDate}
-          disabled={disableDate}
-          onChange={handleDate}
-        />
-        <div className="mt-3 flex w-full flex-wrap items-center justify-center gap-3 p-2">
-          {notAvailableTimes && (
-            <span className="label-text font-bold">
-              No hay disponibilidad, seleccione otra fecha por favor.
-            </span>
-          )}
-          {availableSessionTimes.map((time) => (
-            <span
-              key={Math.random()}
-              className={cn(
-                "badge badge-lg cursor-pointer p-2 hover:bg-accent hover:text-primary",
-                {
-                  "badge-accent text-primary": currentTime === time,
-                  "badge-outline": currentTime !== time,
-                },
-              )}
-              onClick={() => setCurrentTime(time)}
-            >
-              {time}
-            </span>
-          ))}
-        </div>
-      </label>
-
-      <label className="form-control col-span-12 grid w-full lg:col-span-4">
-        <div className="label text-lg font-bold">
-          <span className="label-text">Total asientos</span>
-        </div>
-        <input
-          id="totalSeats"
-          name="totalSeats"
-          value={seats.totalSeats}
-          type="number"
-          className="input input-sm input-bordered w-full"
-          disabled
-        />
-      </label>
-
-      <label className="form-control col-span-12 grid w-full lg:col-span-4">
+      <label className="form-control col-span-12 grid w-full lg:col-span-6">
         <div className="label text-lg font-bold">
           <span className="label-text">Total asientos preferenciales</span>
         </div>
         <input
           id="totalSeatsPreferential"
           name="totalSeatsPreferential"
-          value={seats.totalSeatsPreferential}
           type="number"
           className="input input-sm input-bordered w-full"
-          disabled
+          required
         />
       </label>
-
-      <label className="form-control col-span-12 grid w-full lg:col-span-4">
+      <label className="form-control col-span-12 grid w-full lg:col-span-6">
         <div className="label text-lg font-bold">
           <span className="label-text">Total asientos generales</span>
         </div>
         <input
           id="totalSeatsGeneral"
           name="totalSeatsGeneral"
-          value={seats.totalSeatsGeneral}
-          type="number"
-          className="input input-sm input-bordered w-full"
-          disabled
-        />
-      </label>
-
-      <label className="form-control col-span-12 grid w-full lg:col-span-6">
-        <div className="label text-lg font-bold">
-          <span className="label-text">Precio asientos preferenciales</span>
-        </div>
-        <input
-          id="preferentialPrice"
-          name="preferentialPrice"
           type="number"
           className="input input-sm input-bordered w-full"
           required
         />
       </label>
-
-      <label className="form-control col-span-12 grid w-full lg:col-span-6">
-        <div className="label text-lg font-bold">
-          <span className="label-text">Precio asientos generales</span>
-        </div>
-        <input
-          id="generalPrice"
-          name="generalPrice"
-          type="number"
-          className="input input-sm input-bordered w-full"
-          required
-        />
-      </label>
-
       <div className="col-span-12 mt-6 grid w-full">
         <div className="flex w-full items-center justify-center gap-4">
           <Link
-            href="/dashboard/sessions"
+            href="/dashboard/rooms"
             className="btn btn-error btn-sm min-w-28 text-primary"
           >
             Cancelar
--- app/ui/dashboard/Rooms/components/Create/index.tsx
+++ app/ui/dashboard/Rooms/components/Create/index.tsx
@@ -1,14 +1,10 @@
-import { getAllMovies } from "@/services/movies";
-import { getAllRooms } from "@/services/rooms";
 import DashboardLayout from "../../../DashboardLayout";
-import SessionCreateForm from "./components/CreateForm";
+import RoomCreateForm from "./components/CreateForm";
 
 export default async function CreateRoom() {
-  const movies = JSON.parse(JSON.stringify(await getAllMovies()));
-  const rooms = JSON.parse(JSON.stringify(await getAllRooms()));
   return (
-    <DashboardLayout title="Crear sesión">
-      <SessionCreateForm movies={movies} rooms={rooms} />
+    <DashboardLayout title="Crear sala">
+      <RoomCreateForm />
     </DashboardLayout>
   );
 }
