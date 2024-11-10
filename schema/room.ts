07f07d3a08ad94e22695fd7e61798716f57c4532
YepileF <yepilef@gmail.com>
2024-11-10 10:44:16 -0500

    create: validation inputs

---
+++ schema/room.ts
@@ -0,0 +1,31 @@
+import { SessionRoom } from "@/types/session";
+import { z } from "zod";
+
+const SessionRoomEnum = z.enum([
+  SessionRoom["2D"],
+  SessionRoom["3D"],
+  SessionRoom.IMAX,
+]);
+
+const roomSchema = z.object({
+  name: z
+    .string({
+      required_error: "El título es obligatorio",
+    })
+    .min(1, "El título no puede estar vacío"),
+  room: SessionRoomEnum.default(SessionRoom["2D"]),
+  totalSeatsPreferential: z
+    .number({
+      required_error: "El Total asientos preferenciales es obligatorio",
+    })
+    .int()
+    .positive(
+      "El Total asientos preferenciales debe ser un número entero positivo",
+    ),
+  totalSeatsGeneral: z
+    .number({
+      required_error: "El Total asientos generales es obligatorio",
+    })
+    .int()
+    .positive("El Total asientos generales debe ser un número entero positivo"),
+});
