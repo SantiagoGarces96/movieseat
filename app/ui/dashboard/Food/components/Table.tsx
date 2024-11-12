6251eab90bd1e85cd0ad984f1ebaf2aba3b958ae
YepileF <yepilef@gmail.com>
2024-11-12 17:38:56 -0500

    create: table

--- app/ui/dashboard/Food/components/Table.tsx
+++ app/ui/dashboard/Food/components/Table.tsx
@@ -1,15 +1,20 @@
-import { moviesHeaders } from "@/constants/dashboard/headers";
+import { foodHeaders } from "@/constants/dashboard/headers";
 import ResultCount from "../../ResultCount";
 import ResultSearch from "../../ResultSearch";
 import SortableHeader from "../../SortableHeader";
 import ResultPagination from "../../ResultPagination";
 import { DeleteMovieButton, UpdateMovieButton } from "./Buttons";
-import { getMoviesInDashboard } from "@/services/movies";
 import Image from "next/image";
-import { SpanishMovieStatus } from "@/types/movie";
 import { cn } from "@/utils/cn";
+import { getFood } from "@/services/food";
+import {
+  FoodSize,
+  SpanishFoodSize,
+  SpanishFoodType,
+  SpanishhFoodCategory,
+} from "@/types/food";
 
-export default async function MoviesTable({
+export default async function FoodTable({
   page,
   limit,
   query,
@@ -22,7 +27,7 @@ export default async function MoviesTable({
   sortBy: string;
   order: string;
 }) {
-  const movies = await getMoviesInDashboard(page, limit, query, sortBy, order);
+  const food = await getFood(page, limit, query, sortBy, order);
   const current = parseInt(limit) * parseInt(page);
   return (
     <div className="flex h-full flex-col items-center justify-center">
@@ -30,53 +35,58 @@ export default async function MoviesTable({
         <ResultCount />
         <ResultSearch
           placeholder="Ingrese su busqueda"
-          tooltipText="Puedes buscar por: Titulo, Género, Director y Estado(En Inglés)."
+          tooltipText="Puedes buscar por: Nombre, Categoria y tipo."
         />
       </div>
       <div className="h-full max-h-[75vh] w-full overflow-auto 2xl:hidden">
-        {movies.results.map((data, index) => {
-          const [title, posterURL, genre] = data.titleAndPoster.split("|");
+        {food.results.map((data, index) => {
+          const [name, image, category, type] = data.nameAndImage.split("|");
+          const prasedCategory = category.toUpperCase();
+          const prasedType = type.toUpperCase();
+          const prasedValue = data.size.toUpperCase();
           return (
             <div key={data._id} className="mb-2 w-full rounded-md border p-4">
               <div className="flex w-full items-center justify-between pb-4 text-sm">
                 <p className="w-1/2">
-                  Película {(parseInt(page) - 1) * parseInt(limit) + index + 1}
+                  Comida {(parseInt(page) - 1) * parseInt(limit) + index + 1}
                   <br />
-                  <strong>{data.releaseDate}</strong>
                 </p>
               </div>
               <div className="flex w-full flex-col items-center justify-between gap-3 border-b py-5 md:flex-row md:gap-0">
                 <div className="flex w-full items-center justify-start gap-2 md:w-1/2">
                   <div className="relative h-28 w-20">
-                    <Image
-                      src={posterURL}
-                      alt={title}
-                      fill
-                      className="rounded-md"
-                    />
+                    <Image src={image} alt={name} fill className="rounded-md" />
                   </div>
                   <div className="h-28 w-full max-w-[70%]">
-                    <div className="truncate text-sm font-bold">{title}</div>
-                    <p className="truncate text-xs">{genre}</p>
-                    <p className="truncate text-xs">{data.director}</p>
+                    <div className="truncate text-sm font-bold">{name}</div>
+                    <p className="truncate text-xs capitalize">
+                      {prasedCategory in SpanishhFoodCategory
+                        ? SpanishhFoodCategory[
+                            prasedCategory as keyof typeof SpanishhFoodCategory
+                          ]
+                        : "-"}
+                    </p>
+                    <p className="truncate text-xs capitalize">
+                      {prasedType in SpanishFoodType
+                        ? SpanishFoodType[
+                            prasedType as keyof typeof SpanishFoodType
+                          ]
+                        : "-"}
+                    </p>
                     <div
-                      className={cn("badge badge-xs", {
-                        "badge-info":
-                          data.status.toLowerCase() ===
-                          SpanishMovieStatus.PRE_SALE,
-                        "badge-warning":
-                          data.status.toLowerCase() ===
-                          SpanishMovieStatus.UPCOMING,
-                        "badge-success":
-                          data.status.toLowerCase() ===
-                          SpanishMovieStatus.BILLBOARD,
-                        "badge-ghost":
-                          data.status.toLowerCase() ===
-                          SpanishMovieStatus.ARCHIVED,
+                      className={cn("badge badge-xs capitalize", {
+                        "badge-info": data.size === FoodSize.EXTRALARGE,
+                        "badge-warning": data.size === FoodSize.LARGE,
+                        "badge-success": data.size === FoodSize.SMALL,
                       })}
                     >
-                      {data.status}
+                      {prasedValue in SpanishFoodSize
+                        ? SpanishFoodSize[
+                            prasedValue as keyof typeof SpanishFoodSize
+                          ]
+                        : "-"}
                     </div>
+                    <p className="truncate text-xs font-bold">$ {data.price}</p>
                   </div>
                 </div>
                 <div className="flex h-full max-h-52 w-full flex-col md:w-1/2">
@@ -86,7 +96,7 @@ export default async function MoviesTable({
               </div>
               <div className="flex w-full items-center justify-between pt-4 text-sm">
                 <p>
-                  <strong>{data.duration}</strong> minutos
+                  <strong>{data.availableAmount}</strong> unidades disponibles
                 </p>
                 <div className="flex items-end justify-center gap-3">
                   <DeleteMovieButton id={data._id} />
@@ -102,7 +112,7 @@ export default async function MoviesTable({
           <thead>
             <tr>
               <th></th>
-              {moviesHeaders.map(({ label, value }, index) => (
+              {foodHeaders.map(({ label, value }, index) => (
                 <SortableHeader
                   key={value + index}
                   title={label}
@@ -113,7 +123,7 @@ export default async function MoviesTable({
             </tr>
           </thead>
           <tbody>
-            {movies.results.map((data, index) => (
+            {food.results.map((data, index) => (
               <tr key={"element-" + data._id}>
                 {Object.entries(data).map(([key, value]) => {
                   switch (key) {
@@ -124,33 +134,55 @@ export default async function MoviesTable({
                         </th>
                       );
 
-                    case "titleAndPoster":
-                      const [title, posterURL, genre] = value.split("|");
+                    case "nameAndImage":
+                      const [name, image, category, type] = value.split("|");
+                      const prasedCategory = category.toUpperCase();
+                      const prasedType = type.toUpperCase();
                       return (
                         <td key={key}>
                           <div className="flex items-center gap-3">
                             <div className="avatar">
                               <div className="mask mask-squircle relative h-12 w-12">
-                                <Image src={posterURL} alt={title} fill />
+                                <Image src={image} alt={name} fill />
                               </div>
                             </div>
                             <div className="max-w-56">
-                              <div className="truncate font-bold">{title}</div>
-                              <div
-                                className="tooltip tooltip-bottom tooltip-accent"
-                                data-tip={genre}
-                              >
-                                <p className="max-w-56 truncate">{genre}</p>
-                              </div>
+                              <div className="truncate font-bold">{name}</div>
+                              <p className="max-w-56 truncate capitalize">
+                                {prasedCategory in SpanishhFoodCategory
+                                  ? SpanishhFoodCategory[
+                                      prasedCategory as keyof typeof SpanishhFoodCategory
+                                    ]
+                                  : "-"}
+                              </p>
+                              <p className="max-w-56 truncate capitalize">
+                                {prasedType in SpanishFoodType
+                                  ? SpanishFoodType[
+                                      prasedType as keyof typeof SpanishFoodType
+                                    ]
+                                  : "-"}
+                              </p>
                             </div>
                           </div>
+                        </td>
+                      );
+
+                    case "size":
+                      const prasedValue = value.toUpperCase();
+                      return (
+                        <td key={key} className="capitalize">
+                          {prasedValue in SpanishFoodSize
+                            ? SpanishFoodSize[
+                                prasedValue as keyof typeof SpanishFoodSize
+                              ]
+                            : "-"}
                         </td>
                       );
 
                     default:
                       return (
                         <td key={key}>
-                          {key === "description" || key === "genre" ? (
+                          {key === "description" ? (
                             <div
                               className="tooltip tooltip-accent !z-50"
                               data-tip={value}
@@ -173,16 +205,16 @@ export default async function MoviesTable({
           </tbody>
         </table>
       </div>
-      {movies.totalPages > 0 && (
+      {food.totalPages > 0 && (
         <div className="flex w-full items-center justify-between px-1 py-5">
           <span className="text-base text-gray-400">
-            {(current > movies.totalResults ? movies.totalResults : current) +
+            {(current > food.totalResults ? food.totalResults : current) +
               " of " +
-              movies.totalResults}
+              food.totalResults}
           </span>
           <ResultPagination
             currentPage={parseInt(page)}
-            totalPages={movies.totalPages}
+            totalPages={food.totalPages}
           />
         </div>
       )}
