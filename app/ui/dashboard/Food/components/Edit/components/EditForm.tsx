b4caa2b98e9bfc4c42ed57c15f2bf153fd54c3e0
YepileF <yepilef@gmail.com>
2024-11-12 18:47:48 -0500

    create: food edit form

--- app/ui/dashboard/Food/components/Edit/components/EditForm.tsx
+++ app/ui/dashboard/Food/components/Edit/components/EditForm.tsx
@@ -3,42 +3,23 @@
 import useAlert from "@/app/hooks/useAlert";
 import Alert from "@/app/ui/dashboard/Alert";
 import { initialState } from "@/constants/dashboard/form";
-import { IGenresMovies, ILanguagesMovies, IMovie } from "@/interfaces/movie";
-import { updateMovie } from "@/services/movies";
-import {
-  MovieCreateFormState,
-  MovieStatus,
-  SpanishMovieStatus,
-} from "@/types/movie";
-import { useState } from "react";
 import { useFormState } from "react-dom";
-import CastInput from "../../Input/CastInput";
-import GenreInput from "../../Input/GenreInput";
-import LanguageInput from "../../Input/LanguageInput";
 import Link from "next/link";
 import { Button } from "@/app/ui/dashboard/Button";
+import { IFood } from "@/interfaces/food";
+import {
+  FoodCategory,
+  FoodSize,
+  FoodType,
+  SpanishFoodSize,
+  SpanishFoodType,
+  SpanishhFoodCategory,
+} from "@/types/food";
+import { updateFood } from "@/services/food";
 
-export default function MovieEditForm({
-  genres,
-  languages,
-  movieData,
-}: {
-  genres: IGenresMovies[];
-  languages: ILanguagesMovies[];
-  movieData: IMovie;
-}) {
-  const [currentDate] = new Date(movieData.releaseDate)
-    .toISOString()
-    .split("T");
-  const [state, setState] = useState<MovieCreateFormState>({
-    id: movieData._id,
-    cast: movieData.cast,
-    genre: movieData.genre,
-    language: movieData.language,
-  });
-
+export default function FoodEditForm({ foodData }: { foodData: IFood }) {
   const [stateAction, formAction] = useFormState(
-    updateMovie.bind(null, state),
+    updateFood.bind(null, foodData._id),
     initialState,
   );
 
@@ -53,52 +34,13 @@ export default function MovieEditForm({
       <div className="col-span-12 grid w-full lg:col-span-6">
         <label className="form-control w-full">
           <div className="label text-lg font-bold">
-            <span className="label-text">IMDB ID</span>
+            <span className="label-text">Nombre</span>
           </div>
           <input
-            id="imdb_id"
-            name="imdb_id"
-            type="number"
-            defaultValue={movieData.imdb_id}
-            className="input input-sm input-bordered w-full"
-            required
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label text-lg font-bold">
-            <span className="label-text">Titulo</span>
-          </div>
-          <input
-            id="title"
-            name="title"
+            id="name"
+            name="name"
             type="text"
-            defaultValue={movieData.title}
-            className="input input-sm input-bordered w-full"
-            required
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label text-lg font-bold">
-            <span className="label-text">Director</span>
-          </div>
-          <input
-            id="director"
-            name="director"
-            type="text"
-            defaultValue={movieData.director}
-            className="input input-sm input-bordered w-full"
-            required
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label text-lg font-bold">
-            <span className="label-text">Fecha de estreno</span>
-          </div>
-          <input
-            id="releaseDate"
-            name="releaseDate"
-            type="date"
-            defaultValue={currentDate}
+            defaultValue={foodData.name}
             className="input input-sm input-bordered w-full"
             required
           />
@@ -108,146 +50,144 @@ export default function MovieEditForm({
       <div className="col-span-12 grid w-full lg:col-span-6">
         <label className="form-control w-full">
           <div className="label text-lg font-bold">
+            <span className="label-text">Imagen</span>
+          </div>
+          <input
+            id="image"
+            name="image"
+            type="url"
+            defaultValue={foodData.image}
+            className="input input-sm input-bordered w-full"
+            required
+          />
+        </label>
+      </div>
+
+      <label className="form-control col-span-12 grid w-full lg:col-span-4">
+        <div className="label text-lg font-bold">
+          <span className="label-text">Categoria</span>
+        </div>
+        <select
+          id="category"
+          name="category"
+          className="select select-bordered select-sm w-full capitalize"
+          defaultValue={foodData.category}
+        >
+          <option value="" disabled>
+            Seleccione una categoria
+          </option>
+          {Object.entries(FoodCategory).map(([_, value], index) => (
+            <option key={value + index} value={value}>
+              {
+                SpanishhFoodCategory[
+                  value.toUpperCase() as keyof typeof SpanishhFoodCategory
+                ]
+              }
+            </option>
+          ))}
+        </select>
+      </label>
+
+      <label className="form-control col-span-12 grid w-full lg:col-span-4">
+        <div className="label text-lg font-bold">
+          <span className="label-text">Tipo</span>
+        </div>
+        <select
+          id="type"
+          name="type"
+          className="select select-bordered select-sm w-full capitalize"
+          defaultValue={foodData.type}
+        >
+          <option value="" disabled>
+            Seleccione un tipo
+          </option>
+          {Object.entries(FoodType).map(([_, value], index) => (
+            <option key={value + index} value={value}>
+              {
+                SpanishFoodType[
+                  value.toUpperCase() as keyof typeof SpanishFoodType
+                ]
+              }
+            </option>
+          ))}
+        </select>
+      </label>
+
+      <label className="form-control col-span-12 grid w-full lg:col-span-4">
+        <div className="label text-lg font-bold">
+          <span className="label-text">Tamaño</span>
+        </div>
+        <select
+          id="size"
+          name="size"
+          className="select select-bordered select-sm w-full capitalize"
+          defaultValue={foodData.size}
+        >
+          <option value="" disabled>
+            Seleccione un tamaño
+          </option>
+          {Object.entries(FoodSize).map(([_, value], index) => (
+            <option key={value + index} value={value}>
+              {
+                SpanishFoodSize[
+                  value.toUpperCase() as keyof typeof SpanishFoodSize
+                ]
+              }
+            </option>
+          ))}
+        </select>
+      </label>
+
+      <div className="col-span-12 grid w-full lg:col-span-6">
+        <label className="form-control w-full">
+          <div className="label text-lg font-bold">
             <span className="label-text">Descripcion</span>
           </div>
           <textarea
             id="description"
             name="description"
-            defaultValue={movieData.description}
+            defaultValue={foodData.description}
             className="textarea textarea-bordered textarea-sm w-full"
             required
-            rows={8}
+            rows={4}
           />
         </label>
       </div>
 
-      <CastInput {...{ state, setState }} />
-
-      <GenreInput {...{ state, setState, genresData: genres }} />
-
-      <label className="form-control col-span-12 grid w-full lg:col-span-4">
-        <div className="label text-lg font-bold">
-          <span className="label-text">Trailer</span>
-        </div>
-        <input
-          id="trailer"
-          name="trailer"
-          type="url"
-          defaultValue={movieData.trailer}
-          className="input input-sm input-bordered w-full"
-        />
-      </label>
-
-      <label className="form-control col-span-12 grid w-full lg:col-span-4">
-        <div className="label text-lg font-bold">
-          <span className="label-text">Portada</span>
-        </div>
-        <input
-          id="poster"
-          name="poster"
-          type="url"
-          defaultValue={movieData.poster}
-          className="input input-sm input-bordered w-full"
-          required
-        />
-      </label>
-
-      <label className="form-control col-span-12 grid w-full lg:col-span-4">
-        <div className="label text-lg font-bold">
-          <span className="label-text">Contraportada</span>
-        </div>
-        <input
-          id="backdrop"
-          name="backdrop"
-          type="url"
-          defaultValue={movieData.backdrop}
-          className="input input-sm input-bordered w-full"
-          required
-        />
-      </label>
-
-      <div className="form-control col-span-12 grid w-full lg:col-span-6">
-        <div className="label text-lg font-bold">
-          <span className="label-text">Estado</span>
-        </div>
-        <label className="label cursor-pointer">
-          <span className="label-text capitalize">
-            {SpanishMovieStatus.UPCOMING}
-          </span>
+      <div className="col-span-12 grid w-full lg:col-span-6">
+        <label className="form-control w-full">
+          <div className="label text-lg font-bold">
+            <span className="label-text">Precio</span>
+          </div>
           <input
-            id="status"
-            name="status"
-            type="radio"
-            defaultValue={MovieStatus.UPCOMING}
-            className="radio checked:bg-accent"
-            defaultChecked={movieData.status === MovieStatus.UPCOMING}
+            id="price"
+            name="price"
+            type="number"
+            defaultValue={foodData.price}
+            className="input input-sm input-bordered w-full"
+            required
           />
         </label>
-        <label className="label cursor-pointer">
-          <span className="label-text capitalize">
-            {SpanishMovieStatus.PRE_SALE}
-          </span>
+
+        <label className="form-control w-full">
+          <div className="label text-lg font-bold">
+            <span className="label-text">Cantidad disponible</span>
+          </div>
           <input
-            id="status"
-            name="status"
-            type="radio"
-            defaultValue={MovieStatus.PRE_SALE}
-            className="radio checked:bg-accent"
-            defaultChecked={movieData.status === MovieStatus.PRE_SALE}
+            id="availableAmount"
+            name="availableAmount"
+            type="number"
+            defaultValue={foodData.availableAmount}
+            className="input input-sm input-bordered w-full"
+            required
           />
         </label>
-        <label className="label cursor-pointer">
-          <span className="label-text capitalize">
-            {SpanishMovieStatus.BILLBOARD}
-          </span>
-          <input
-            id="status"
-            name="status"
-            type="radio"
-            defaultValue={MovieStatus.BILLBOARD}
-            className="radio checked:bg-accent"
-            defaultChecked={movieData.status === MovieStatus.BILLBOARD}
-          />
-        </label>
-      </div>
-      <div className="col-span-12 grid lg:col-span-6">
-        <LanguageInput {...{ state, setState, languageData: languages }} />
-        <div className="flex w-full gap-3">
-          <label className="form-control w-full">
-            <div className="label text-lg font-bold">
-              <span className="label-text">Duración</span>
-            </div>
-            <label className="input input-sm input-bordered flex items-center gap-2">
-              <input
-                id="duration"
-                name="duration"
-                type="number"
-                defaultValue={movieData.duration}
-                className="w-full grow"
-                required
-              />
-              <span className="font-bold text-gray-400">Min</span>
-            </label>
-          </label>
-          <label className="flex w-full items-center justify-center gap-3">
-            <div className="label text-lg font-bold">
-              <span className="label-text">Subtitulos</span>
-            </div>
-            <input
-              id="subtitles"
-              name="subtitles"
-              type="checkbox"
-              className="toggle toggle-accent toggle-sm"
-              defaultChecked={movieData.subtitles}
-            />
-          </label>
-        </div>
       </div>
 
       <div className="col-span-12 mt-6 grid w-full">
         <div className="flex w-full items-center justify-center gap-4">
           <Link
-            href="/dashboard/movies"
+            href="/dashboard/food"
             className="btn btn-error btn-sm min-w-28 text-primary"
           >
             Cancelar
--- app/ui/dashboard/Food/components/Edit/index.tsx
+++ app/ui/dashboard/Food/components/Edit/index.tsx
@@ -1,25 +1,19 @@
 import DashboardLayout from "../../../DashboardLayout";
 import { notFound } from "next/navigation";
-import { getGenres, getLanguages, getMovieById } from "@/services/movies";
 import MovieEditForm from "./components/EditForm";
+import { getFoodById } from "@/services/food";
+import FoodEditForm from "./components/EditForm";
 
 export default async function EditFood({ id }: { id: string }) {
-  const movie = await getMovieById(id);
+  const food = await getFoodById(id);
 
-  if (!movie) {
+  if (!food) {
     notFound();
   }
 
-  const genresData = await getGenres();
-  const languagesData = await getLanguages();
-
   return (
     <DashboardLayout title="Editar sesión">
-      <MovieEditForm
-        genres={genresData}
-        languages={languagesData}
-        movieData={movie}
-      />
+      <FoodEditForm foodData={food} />
     </DashboardLayout>
   );
 }
